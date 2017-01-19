e0c97b560f498b2506b83e2f23f389ee05ad3be0
adjivas <adjivas@users.noreply.github.com>
2017-01-19 15:22:01 +0100

    add enum mouse

--- mod.rs
+++ mod.rs
@@ -23,8 +23,7 @@ pub const WHITE: [libc::c_uchar; 3] = [255, 255, 255];
 pub const DEFAULT_FOREGROUND: [libc::c_uchar; 3] = BLACK;
 pub const DEFAULT_BACKGROUND: [libc::c_uchar; 3] = WHITE;
 
-#[repr(u8)]
-#[derive(Clone, Copy, Debug)]
+#[repr(u32)]
 pub enum Attribute {
     None = 0x00,
     Dim = 0x02,
@@ -36,7 +35,7 @@ pub enum Attribute {
     Hidden = 0x40,
 }
 
-#[repr(u8)]
+#[repr(u32)]
 pub enum Cardinal {
     UpperLeft = 0,
     UpperMiddle = 1,
@@ -55,7 +54,7 @@ pub struct Position {
     pub cartesian: [libc::c_ushort; 2],
 }
 
-#[repr(u8)]
+#[repr(u32)]
 pub enum Sheet {
     None = b'_',
     Bust = b'b',
@@ -67,7 +66,7 @@ pub struct Tuple {
     pub emotion: Emotion,
 }
 
-#[repr(u8)]
+#[repr(u32)]
 pub enum Part {
     None = b'_',
     ArmLeft = b'a',
@@ -96,7 +95,7 @@ pub enum Part {
     WoolBall = b'w',
 }
 
-#[repr(u8)]
+#[repr(u32)]
 pub enum Emotion {
     None = b'_',
     Angry = b'a',
@@ -112,7 +111,7 @@ pub enum Emotion {
     Surprised = b'u',
 }
 
-#[repr(u8)]
+#[repr(u32)]
 pub enum Relative {
     Top = 0,
     Bottom = 1,
@@ -130,6 +129,122 @@ pub struct Character {
     pub background: [libc::c_uchar; 3],
     /// Glyph.
     pub glyph: libc::c_uint,
+}
+
+#[repr(u32)]
+pub enum Mouse {
+    /// The left mouse button is pressed.
+    Left = 0,
+    /// The right mouse button is pressed.
+    Right = 1,
+    /// The mouse wheel button is pressed.
+    Wheel = 2,
+    /// The mouse wheel is going up.
+    WheelUp = 64,
+    /// The mouse wheel is going down.
+    WheelDown = 65,
+    /// The left mouse button is held while moving pointer.
+    LeftDrag = 32,
+    /// The wheel mouse button is held while moving pointer.
+    WheelDrag = 33,
+    /// The right mouse button is held while moving pointer.
+    RightDrag = 34,
+    /// The left mouse button is pressed while helding Shift.
+    ShiftLeft = 4,
+    /// The wheel mouse button is pressed while helding Shift.
+    ShiftWheel = 5,
+    /// The right mouse button is pressed while helding Shift.
+    ShiftRight = 6,
+    /// The left mouse button and Shift are held while moving pointer.
+    ShiftLeftDrag = 36,
+    /// The wheel mouse button and Shift are held while moving pointer.
+    ShiftWheelDrag = 37,
+    /// The right mouse button and Shift are held while moving pointer.
+    ShiftRightDrag = 38,
+    /// The left mouse button is pressed while helding Ctrl
+    CtrlLeft = 16,
+    /// The wheel mouse button is pressed while helding Ctrl
+    CtrlWheel = 17,
+    /// The right mouse button is pressed while helding Ctrl
+    CtrlRight = 18,
+    /// The mouse wheel is going up while helding Ctrl
+    CtrlWheelUp = 80,
+    /// The mouse wheel is going down while helding Ctrl
+    CtrlWheelDown = 81,
+    /// The left mouse button and Ctrl are held while moving pointer
+    CtrlLeftDrag = 48,
+    /// The wheel mouse button and Ctrl are held while moving pointer
+    CtrlWheelDrag = 49,
+    /// The right mouse button and Ctrl are held while moving pointer
+    CtrlRightDrag = 50,
+    /// The left mouse button is pressed while Ctrl and Shift are held
+    ShiftCtrlLeft = 20,
+    /// The wheel mouse button is pressed while Ctrl and Shift are held
+    ShiftCtrlWheel = 21,
+    /// The right mouse button is pressed while Ctrl and Shift are held
+    ShiftCtrlRight = 22,
+    /// The left mouse button, Ctrl and Shift are held while moving pointer
+    ShiftCtrlLeftDrag = 52,
+    /// The wheel mouse button, Ctrl and Shift are held while moving pointer
+    ShiftCtrlWheelDrag = 53,
+    /// The right mouse button, Ctrl and Shift are held while moving pointer
+    ShiftCtrlRightDrag = 54,
+    /// The left mouse button is pressed while helding Command
+    CmdLeft = 8,
+    /// The wheel mouse button is pressed while helding Command
+    CmdWheel = 9,
+    /// The right mouse button is pressed while helding Command
+    CmdRight = 10,
+    /// The mouse wheel is going up while helding Command
+    CmdWheelUp = 72,
+    /// The mouse wheel is going down while helding Command
+    CmdWheelDown = 73,
+    /// The left mouse button and Command are held while moving pointer
+    CmdLeftDrag = 40,
+    /// The wheel mouse button and Command are held while moving pointer
+    CmdWheelDrag = 41,
+    /// The right mouse button and Command are held while moving pointer
+    CmdRightDrag = 42,
+    /// The left mouse button is pressed while helding Command and Shift
+    CmdShiftLeft = 12,
+    /// The wheel mouse button is pressed while helding Command and Shift
+    CmdShiftWheel = 13,
+    /// The right mouse button is pressed while helding Command and Shift
+    CmdShiftRight = 14,
+    /// The left mouse button, Shift and Command are held while moving pointer
+    CmdShiftLeftDrag = 44,
+    /// The wheel mouse button, Shift and Command are held while moving pointer
+    CmdShiftWheelDrag = 45,
+    /// The right mouse button, Shift and Command are held while moving pointer
+    CmdShiftRightDrag = 46,
+    /// The left mouse button is pressed while helding Command and Ctrl
+    CmdCtrlLeft = 24,
+    /// The wheel mouse button is pressed while helding Command and Ctrl
+    CmdCtrlWheel = 25,
+    /// The right mouse button is pressed while helding Command and Ctrl
+    CmdCtrlRight = 26,
+    /// The mouse wheel is going up while helding Command and Ctrl
+    CmdCtrlWheelUp = 88,
+    /// The mouse wheel is going down while helding Command and Ctrl
+    CmdCtrlWheelDown = 89,
+    /// The left mouse button, Ctrl and Command are held while moving pointer
+    CmdCtrlLeftDrag = 56,
+    /// The wheel mouse button, Ctrl and Command are held while moving pointer
+    CmdCtrlWheelDrag = 57,
+    /// The right mouse button, Ctrl and Command are held while moving pointer
+    CmdCtrlRightDrag = 58,
+    /// The left mouse button is pressed while helding Command, Shift and Ctrl
+    CmdShiftCtrlLeft = 28,
+    /// The wheel mouse button is pressed while helding Command, Shift and Ctrl
+    CmdShiftCtrlWheel = 29,
+    /// The right mouse button is pressed while helding Command, Shift and Ctrl
+    CmdShiftCtrlRight = 30,
+    /// The left mouse button, Ctrl, Shift and Command are held while moving pointer
+    CmdShiftCtrlLeftDrag = 60,
+    /// The wheel mouse button, Ctrl, Shift and Command are held while moving pointer
+    CmdShiftCtrlWheelDrag = 61,
+    /// The right mouse button, Ctrl, Shift and Command are held while moving pointer
+    CmdShiftCtrlRightDrag = 62,
 }
 
 #[repr(C)]
